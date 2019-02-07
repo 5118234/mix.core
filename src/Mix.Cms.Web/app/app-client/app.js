ab022fa7a944b8ba4c5991ab60642f3d124cadaa
Smilefounder <i.love.to.smile.around@gmail.com>
2019-02-07 16:48:24 +0000

    Deploy website
    Deploy website version based on b284b9ed7123e169b8e8c804fd808183149e51e1

--- src/Mix.Cms.Web/app/app-client/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var app = angular.module('MixClient', ['ngRoute', 'LocalStorageModule', 'components', 'ngSanitize']);
-var serviceBase = '';
-
-var modules = angular.module('components', []);
--- src/Mix.Cms.Web/app/app-init/app.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-var app = angular.module('MixPortal',
-    ['ngRoute', 'ngFileUpload', 'LocalStorageModule']);
-    var modules = angular.module('components', []);
